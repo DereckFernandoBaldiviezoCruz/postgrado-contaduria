c03d23cfa2fd460f6656f93a18f5d6f6256d7665
DereckFernandoBaldiviezoCruz <dereck120120120@gmail.com>
2026-04-15 17:51:47 -0400

    1.0.1

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,13 +1,22 @@
-const mysql = require("mysql2/promise");
+const mysql = require('mysql2/promise');
+const path = require('path');
+const fs = require('fs');
+const caPath = path.join(__dirname, 'ca.pem');
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "maestrias",
+  host: 'mysql-2bfe0608-postgrado-contaduria-usfx.b.aivencloud.com',
+  port: 19202,
+  user: 'avnadmin',
+  password: 'AVNS_8Pk-jC0Ll43h_BSYMbr',
+  database: 'defaultdb',
+
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+
+  ssl: {
+    ca: fs.readFileSync(caPath),
+  },
 });
 
 module.exports = pool;
